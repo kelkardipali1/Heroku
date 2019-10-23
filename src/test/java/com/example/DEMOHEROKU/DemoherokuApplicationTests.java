a38cd5a0d6bfa7083f35efb0105fce8861a93c81
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-10-23 13:25:14 +0530

    first commit

---
+++ src/test/java/com/example/DEMOHEROKU/DemoherokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DEMOHEROKU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoherokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
